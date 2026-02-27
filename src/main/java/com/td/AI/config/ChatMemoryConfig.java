08f441b27b146f41062778cefc179ff75380f29c
RAJUNAIDUJAVA <rajunaidusingampalli48@gmail.com>
2026-02-27 15:59:50 +0530

    Spring AI: JDBC Chat memory implemeted for storing messages permanently

---
+++ src/main/java/com/td/AI/config/ChatMemoryConfig.java
@@ -0,0 +1,18 @@
+package com.td.AI.config;
+
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatMemoryConfig {
+    @Bean
+    public ChatMemory chatMemory(JdbcChatMemoryRepository repository){
+
+        return MessageWindowChatMemory.builder().chatMemoryRepository(repository).maxMessages(100).build();
+
+    }
+
+}
---
+++ src/main/java/com/td/AI/controller/JdbcChatMemoryAdvisor.java
@@ -0,0 +1,28 @@
+package com.td.AI.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("jdbc")
+public class JdbcChatMemoryAdvisor {
+
+    ChatClient chatClient;
+
+    public JdbcChatMemoryAdvisor (ChatClient.Builder builder,JdbcChatMemoryRepository repository){
+
+        ChatMemory chatMemory= MessageWindowChatMemory.builder().chatMemoryRepository(repository).maxMessages(100).build();
+        this.chatClient= builder.defaultAdvisors(PromptChatMemoryAdvisor.builder(chatMemory).build()).build();
+    }
+    @GetMapping("/call")
+    public String getResponse(){
+        String message = "explain spring security";
+        return chatClient.prompt(message).call().content();
+    }
+}
